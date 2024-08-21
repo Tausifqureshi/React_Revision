4d053409417ea89e889abea9b46b4692cdb60542
Tausifqureshi <tausifqureshi504@gmail.com>
2024-08-21 15:13:28 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 // import React from 'react'
-import Action from "./inroduction/Action"
 export default function App() {
   return (
  <div className="container">
-  {/* <h1>React Code Run</h1> */}
-  <Action/>
+  <h1>React code run</h1>
  </div>
   )
 }
--- src/inroduction/Action.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Action() {
-  return (
-    <div>
-      <p>MY Code Is veary Important</p>
-    </div>
-  )
-}
