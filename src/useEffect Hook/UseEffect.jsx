f894f195aa04a46e1230d29e2967e5d288516d8a
Tausifqureshi <tausifqureshi504@gmail.com>
2024-09-09 14:38:02 +0530

    Updated

--- src/useEffect Hook/UseEffect.jsx
+++ src/useEffect Hook/UseEffect.jsx
@@ -1,6 +1,9 @@
+// useEffect ka use API se data fetch karne ya eventListener add karne ke liye bhi useEffect ka use karna padta hai. ya koi sideEffect parform karne ke liye bhi useEffect ka use karte hai.
+
 import React from 'react'
 
 function UseEffect() {
+
   return (
     <h1>
     UseEffect
