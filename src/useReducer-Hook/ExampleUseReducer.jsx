78f11336ed706339c14bdaf8cc0725c68172ad9a
Tausifqureshi <tausifqureshi504@gmail.com>
2024-09-12 07:38:58 +0530

    useReducer-Hook

--- src/useReducer-Hook/ExampleUseReducer.jsx
+++ src/useReducer-Hook/ExampleUseReducer.jsx
@@ -1,9 +1,36 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
 function ExampleUseReducer() {
-  return <div>
-    
-  </div>;
+  const [counter, setCounter] = useState(0);
+  const increaseHandle = () => {
+    //    setCounter(counter + 1);
+
+    setCounter((prev) => prev + 1); //is tara se bhi use kar sakte hai.
+  };
+
+  const resetHandle = () => {
+    setCounter(0);
+  };
+  const decreaseHandle = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <div>
+      <h1>Application</h1>
+      <h1>Counter {counter}</h1>
+      <div>
+        <button style={{ margin: "0 10px" }} onClick={increaseHandle}>
+          Increase
+        </button>
+        <button style={{ margin: "0 10px" }} onClick={resetHandle}>
+          Reset
+        </button>
+        <button style={{ margin: "0 10px" }} onClick={decreaseHandle}>
+          Decrease
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ExampleUseReducer;
--- src/useReducer-Hook/UseReducer.jsx
+++ src/useReducer-Hook/UseReducer.jsx
@@ -1,15 +1,20 @@
+//userReducer-Hook ka use ---> is use ham karte hai complex state ko mange karne ke liye.
+
+
+
 import React from 'react'
 import ExampleUseReducer from './ExampleUseReducer'
 
 function UseReducer() {
 
   return (
-    <>
+    <div style={{textAlign:"center"}}>
 
-    <h2>Use Reducer Hook</h2>
+    <h2 style={{backgroundColor: "#929292"}}>Use Reducer Hook</h2>
+    <hr />
     <ExampleUseReducer />
-
-    </>
+       
+    </div>
   )
 }
 
