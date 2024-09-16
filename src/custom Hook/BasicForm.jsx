985279c9695a0d3fbfb33668f09d70ef34773901
Tausifqureshi <tausifqureshi504@gmail.com>
2024-09-16 11:19:20 +0530

    custom-hook

--- src/custom Hook/BasicForm.jsx
+++ src/custom Hook/BasicForm.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import useLocalStorage from './useLocalStorage';
+import React from "react";
+import useLocalStorage from "./useLocalStorage";
+import styles from "./BasicForm.module.css";
 
 function BasicForm() {
-  const [firstName, setFirstName] = useLocalStorage('firstName', '');
+  const [firstName, setFirstName] = useLocalStorage("firstName", "");
+  const [names, setNames] = useLocalStorage("names", []);
 
-  const handleChange = (e) => {
+  const handleInput = (e) => {
     setFirstName(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Submitted: ${firstName}`);
+    const trimmedName = firstName.trim();
+    if (trimmedName.length === 0) {
+      alert("Please enter a valid name.");
+      return;
+    }
+    
+    setNames((prevState) => [...prevState, trimmedName]);
+    setFirstName("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        First Name:
-        <input type="text" value={firstName} onChange={handleChange} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <label>First Name:</label>
+        <input type="text" value={firstName} onChange={handleInput} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <h3>Submitted Names:</h3>
+        <ul>
+          {names.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
