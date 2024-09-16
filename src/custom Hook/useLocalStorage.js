6113c5e7b6f83dacd460df89ac49528c1ae04bb4
Tausifqureshi <tausifqureshi504@gmail.com>
2024-09-16 13:43:07 +0530

    Custom-Hooks

--- src/custom Hook/useLocalStorage.js
+++ src/custom Hook/useLocalStorage.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 
 export default function useLocalStorage(key, initialValue) {
-  const [names, setNames] = useState('');
-  const [state, setState] = useState(() => {
+  const [names, setNames] = useState(""); //setName ke liye ye state hai.
+  const [state, setState] = useState(() => { //user ki input lene ke liye ye state hai. 
     try {
       return JSON.parse(localStorage.getItem(key)) || initialValue;
     } catch (error) {
@@ -11,7 +11,8 @@ export default function useLocalStorage(key, initialValue) {
     }
   });
 
-  useEffect(() => {// Update localStorage Jitne bar Firstname change hoga utne bar localStorage me data change hoga automatically os liye ham is useEffect ka use karte hai.  React ka useEffect hook side-effects ko handle karne ke liye use hota hai, jaise DOM updates, API calls, ya localStorage updates.
+  useEffect(() => {
+    // Update localStorage Jitne bar Firstname change hoga utne bar localStorage me data change hoga automatically os liye ham is useEffect ka use karte hai.  React ka useEffect hook side-effects ko handle karne ke liye use hota hai, jaise DOM updates, API calls, ya localStorage updates.
     try {
       localStorage.setItem(key, JSON.stringify(state));
     } catch (error) {
