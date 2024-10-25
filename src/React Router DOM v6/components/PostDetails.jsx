e4b22bfe0be8dccbdb3bb6b0cff4b8a6881d0ffc
Tausifqureshi <tausifqureshi504@gmail.com>
2024-10-25 17:30:41 +0530

    Loder

--- src/React Router DOM v6/components/PostDetails.jsx
+++ src/React Router DOM v6/components/PostDetails.jsx
@@ -4,10 +4,11 @@ import { useParams } from "react-router-dom";
 function PostDetails() {
     const url = "https://jsonplaceholder.typicode.com/users"
 
-    // const params= useParams();
+    const params= useParams();
     const {id}= useParams(); // useParams se destructuring karke URL se id nikal rahe hain, jo React Router ke routes me defined hai. Agar route me id ka naam kuch aur hoga, to useParams me bhi wahi naam dena hoga aur PostDetails component me bhi wahi naam use karna padega.
 
-    // console.log(id);
+    console.log(id);
+    console.log(params);
     const [data, setData] = useState(null);  
     async function fetchData() {
       try {
