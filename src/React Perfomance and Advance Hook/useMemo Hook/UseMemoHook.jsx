e5cb3b93d621e9e5a5816944e58844f41f80efab
Tausifqureshi <tausifqureshi504@gmail.com>
2024-10-29 23:15:31 +0530

    UseMemo Ho9ook

--- src/React Perfomance and Advance Hook/useMemo Hook/UseMemoHook.jsx
+++ src/React Perfomance and Advance Hook/useMemo Hook/UseMemoHook.jsx
@@ -3,11 +3,13 @@ import Demo from "./Demo";
 import UseMemoExample1 from "./UseMemoExample1";
 
 function UseMemoHook() {
+  // useMemo Hook to increase performance in react Application.
+  
   return <div>
    <h1> UseMemoHook </h1>
    {/* <Demo /> */}
    <UseMemoExample1 />
-   
+
   </div>;
 }
 
