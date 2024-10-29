ffc5345faa1573abc9435b6b85daa6b4bf6e0d69
Tausifqureshi <tausifqureshi504@gmail.com>
2024-10-29 22:55:20 +0530

    useMemo

---
+++ src/React Perfomance and Advance Hook/useMemo Hook/UseMemoExample1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function UseMemoExample1() {
+  return <div>
+ <h1>  UseMemoExample1 </h1>
+ 
+  </div>;
+}
+
+export default UseMemoExample1;
--- src/React Perfomance and Advance Hook/useMemo Hook/UseMemoHook.jsx
+++ src/React Perfomance and Advance Hook/useMemo Hook/UseMemoHook.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Demo from "./Demo";
+import UseMemoExample1 from "./UseMemoExample1";
 
 function UseMemoHook() {
   return <div>
    <h1> UseMemoHook </h1>
-   <Demo />
+   {/* <Demo /> */}
+   <UseMemoExample1 />
+   
   </div>;
 }
 
