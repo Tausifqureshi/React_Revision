ed7f66661c06cd0ede75555794a52763991a2c88
Tausifqureshi <tausifqureshi504@gmail.com>
2024-10-30 17:07:36 +0530

    use Call Back

--- src/React Perfomance and Advance Hook/useMemo Hook/UseMemoExample1.jsx
+++ src/React Perfomance and Advance Hook/useMemo Hook/UseMemoExample1.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UseMemoExample1() {
-  return <div>
- <h1>  UseMemoExample1 </h1>
- 
-  </div>;
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  function counterIncrease() {
+    setCounter((prevState) => prevState + 1);
+  }
+
+  const countNumber = (num)=>{
+    console.log("Count Function Called", num);
+    for (let i = 0; i < 1000; i++) {}
+    
+    return num;
+  }
+
+  const saveCountValue = countNumber(counter);
+
+
+  return (
+    <div>
+      <h1> UseMemoExample1 </h1> <br />
+      <p> My Number : {saveCountValue} </p>
+      <button onClick={counterIncrease} style={{ background: "red", display: "block", margin: "auto", }}>
+        Counter
+      </button> <br /> <br />
+
+      <button onClick={()=>setToggle(!toggle)} style={{ background: "#323232", display: "block", margin: "auto", }}>
+        {toggle? "You Clicked": "Please Cliked Now"}
+
+      </button>
+
+
+
+
+
+
+
+    </div>
+  );
 }
 
 export default UseMemoExample1;
--- src/React Perfomance and Advance Hook/useMemo Hook/UseMemoHook.jsx
+++ src/React Perfomance and Advance Hook/useMemo Hook/UseMemoHook.jsx
@@ -4,9 +4,9 @@ import UseMemoExample1 from "./UseMemoExample1";
 
 function UseMemoHook() {
   // useMemo Hook to increase performance in react Application.
-  
+
   return <div>
-   <h1> UseMemoHook </h1>
+   {/* <h1> UseMemoHook </h1> */}
    {/* <Demo /> */}
    <UseMemoExample1 />
 
