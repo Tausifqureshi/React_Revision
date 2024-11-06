04261211bd0713c0e29227d00034ca00899f7488
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-06 10:23:17 +0530

    Additions

--- src/React Perfomance and Advance Hook/useMemo Hook/useMemoExample.jsx
+++ src/React Perfomance and Advance Hook/useMemo Hook/useMemoExample.jsx
@@ -16,12 +16,18 @@ function UseMemoExample() {
 
 
   function extraComponets(add){ //ek paarmeter lera hai ye 
-    console.log("Extra Componet");
+    console.log("Extra Componet", add);
     for(let i=0; i< 1000000; i++){} // Dynamic loops ya intensive calculations performance pe impact daal sakti hain. Is problem ko solve karne ke liye useMemo ka use kiya jata hai taaki result ko cache karke unnecessary re-calculations avoid ki ja sakein, aur overall performance improve ho.
-
+  return add; // add return ho jaye ga;
   }
 
+  let number = extraComponets(add);  // extraCompoents Function ke argument me add pass kar re hai add upper wali value hai jab ham addtion kar re ge jo argument me value diya hai ohi value h1 number wale me show hogi.
+  
+
   return <div>
+  <h1> ExtranComponents {number} </h1> 
+  {/* Functon Print */}
+
   <h1> Addition Number: {add} </h1>
    <button type="button" onClick={btnHandle} style={{display: "block", margin: "auto"}}> Addition </button><br /><br />
    <button type="button" onClick={btnMinus} style={{display: "block", margin: "auto"}}> Minus </button>
