923f631fcfc8338bf5dee0d665921993a36bbf0f
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-06 10:14:53 +0530

    UseMempo

--- src/React Perfomance and Advance Hook/useMemo Hook/useMemoExample.jsx
+++ src/React Perfomance and Advance Hook/useMemo Hook/useMemoExample.jsx
@@ -6,8 +6,6 @@ function UseMemoExample() {
   function btnHandle(){
     setAdd(add + 1);
   }
-
-
   function btnMinus (){
     if(add > 0 ){
       setAdd( add- 1)
@@ -15,11 +13,19 @@ function UseMemoExample() {
     }
   }
 
+
+
+  function extraComponets(add){ //ek paarmeter lera hai ye 
+    console.log("Extra Componet");
+    for(let i=0; i< 1000000; i++){} // Dynamic loops ya intensive calculations performance pe impact daal sakti hain. Is problem ko solve karne ke liye useMemo ka use kiya jata hai taaki result ko cache karke unnecessary re-calculations avoid ki ja sakein, aur overall performance improve ho.
+
+  }
+
   return <div>
   <h1> Addition Number: {add} </h1>
    <button type="button" onClick={btnHandle} style={{display: "block", margin: "auto"}}> Addition </button><br /><br />
    <button type="button" onClick={btnMinus} style={{display: "block", margin: "auto"}}> Minus </button>
-    </div>;
+  </div>;
 
 }   
  
