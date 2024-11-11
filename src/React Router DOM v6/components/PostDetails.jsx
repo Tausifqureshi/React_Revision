e66291093d14f85f90fa0ee8a7f26eef9efe7cf8
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-11 09:39:39 +0530

    Redux

--- src/React Router DOM v6/components/PostDetails.jsx
+++ src/React Router DOM v6/components/PostDetails.jsx
@@ -8,7 +8,7 @@ function PostDetails() {
     const {id}= useParams(); // useParams se destructuring karke URL se id nikal rahe hain, jo React Router ke routes me defined hai. Agar route me id ka naam kuch aur hoga, to useParams me bhi wahi naam dena hoga aur PostDetails component me bhi wahi naam use karna padega.
   
     console.log(id);
-    console.log(params);
+    console.log(params);  
     const [data, setData] = useState(null);  
     async function fetchData() {
       try {
