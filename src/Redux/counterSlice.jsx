2a3802df738dbf0f7c8d1f6bc27ca4debc88f065
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-15 00:09:46 +0530

    Redux

--- src/Redux/counterSlice.jsx
+++ src/Redux/counterSlice.jsx
@@ -19,22 +19,24 @@ export const counterSlice = createSlice({
     
     // Increment reducer: `count` ko 1 se badhata hai
     increment: (state, action) => {  // state ko increment kar raha hai
-      
+
       // State ko shallow copy karke `count` me 1 add karke naya state return kar rahe hain
       return { ...state, count: state.count + 1 }; // `count` ki value ko badha rahe hain
     },
 
     // Decrement reducer: `count` ko 1 se kam karta hai
-    decrement: (state, action) => {
+    decrement: (state, action) => { // state ko decrement kar raha hai
+
       // Agar `count` 0 se bada hai to 1 minus karta hai, warna state change nahi hoti
-      if (state.count > 0) {
+      if (state.count > 0) { 
         return { ...state, count: state.count - 1 }; // `count` ko decrease kar rahe hain
       }
       return state; // Agar `count` 0 hai to wahi state return kar dete hain bina kisi change ke
     },
 
     // Reset reducer: `count` ko dobara 0 par set kar deta hai
-    reset: (state, action) => {
+    reset: (state, action) => {  // state ko reset kar raha hai
+
       return { ...state, count: 0 }; // State ko reset karke count ko 0 set kar rahe hain
     },
   },
