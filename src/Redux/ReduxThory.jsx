218ee2361fd6fa38f8e61cea6c81d3e1bc23728a
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-15 23:46:18 +0530

    Redux

---
+++ src/Redux/ReduxThory.jsx
@@ -0,0 +1,31 @@
+// =================================================== Redux ===================================================== //
+// ChatGPT Se Liya Hua Syntex ---> Redux ek JavaScript state management library hai jo primarily React applications mein use hoti hai, lekin ye kisi bhi JavaScript framework ke sath use ki ja sakti hai. Redux ka main purpose hota hai app ke state ko centralized aur predictable tarike se manage karna.
+
+// ============================================== Redux  kya hai ================================================= //
+// ChatGPT Se Liya Hua Syntex ---> Redux kya hai.
+// Redux ek predictable state container hai jo JavaScript applications ke liye banaya gaya hai. Yeh aapko ek centralized store provide karta hai, jismein aapka poora application ka state hota hai. Jab koi bhi component ko state ya data ka access ya update chahiye hota hai, toh wo Redux ke through is central store se interact karta hai
+
+// ============================================= Redux ka History ================================================ //
+// ChatGPT Se Liya Hua Syntex ---> Redux ka History.
+// Redux ko 2015 me Dan Abramov aur Andrew Clark ne develop kiya tha. Redux ka creation React ke ek major problem ko solve karne ke liye kiya gaya tha: state management. React me state ko manage karna kabhi kabhi complex ho sakta tha jab app bada ho jaata tha, aur components ke beech data share karna difficult hota tha. Redux ne is issue ko solve kiya aur ek predictable way me state ko manage kiya jisme ek centralized store rakha jaata hai.
+
+
+// ========================================== Redux ki Basic Concepts ============================================ //
+// ChatGPT Se Liya Hua Syntex ---> Redux ki Basic Concepts.
+
+// 1. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
