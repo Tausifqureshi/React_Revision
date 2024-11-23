119c2ad5051f5a23e997e7163c28d14ccebb543a
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-23 22:24:53 +0530

    Redux Thunk

--- src/Redux Thunk/ReduxThunk.jsx
+++ src/Redux Thunk/ReduxThunk.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReduxThunkExample from "./ReduxThunkExample";
 
 function ReduxThunk() {
+    
   return <div>
     <h1> Redux Thunk </h1>
     <ReduxThunkExample />
   </div>;
+
 }
 
 export default ReduxThunk;
