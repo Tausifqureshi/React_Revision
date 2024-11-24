b6ed9741f9ccdd498e94f1943e807ffc668d509d
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-24 11:55:21 +0530

    Redux

--- src/Redux Thunk/Thory.jsx
+++ src/Redux Thunk/Thory.jsx
@@ -1,6 +1,9 @@
 // ================================================ Redux Thunk ================================================== //
 // ChatGPT Se Liya Hua Syntex --->
 
+// react form hook 
+// validtion --> yap library 
+
 
 
 
