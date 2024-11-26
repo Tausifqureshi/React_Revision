de33d4292354a3439600dd2cc6be3f95b072bffe
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-26 11:25:57 +0530

    Redux

--- src/Redux Thunk/ListTodos.jsx
+++ src/Redux Thunk/ListTodos.jsx
@@ -8,9 +8,9 @@ function ListTodos() {
   const todos = useSelector((state) => state.myExampleTodos.data); // Todos ko store se select karenge
   console.log("initial state mil ri hai", todos);
 
-  // useSelector((state)=>{
-  //     console.log("state mil ri hai", state);
-  //   })
+  useSelector((state)=>{
+      console.log("state mil ri hai", state); // jo hamne todos db.json ka datat push kiya hai na slice me ohi data mile ga yaha upper todos me us me de [data, error, loading] us ko destucturing kar ke nikal sakte hai.
+    })
   
   useEffect(() => {
     dispatch(fetchDatat()); // Thunk function ko dispatch karenge
