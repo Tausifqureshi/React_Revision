9aff7163bd683d76d37555f0a46efc189b783a15
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-26 19:54:18 +0530

    Redux

--- src/Redux Thunk/TodoSingle.jsx
+++ src/Redux Thunk/TodoSingle.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-function TodoSingle({id, title, completed}) {
-  return <div>
-       <p>id : {id}</p>
-       <p>title: {title}</p>
-       <p>completed: {completed}</p>
-  </div>;
+function TodoSingle({ id, title, completed }) {
+  return (
+    <>
+    <div
+      style={{ border: "1px solid #323232", width: "50%", margin: "2rem auto", padding: "2rem"}}
+    >
+      <p>id: {id}</p>
+      <p>title: {title}</p>
+      <p>completed: {completed ? "completed" : "not completed"}</p>
+     <div  style={{display: "flex", justifyContent: "center", gap: "1rem" }}>
+     <button> Delete</button>
+     <button> Toggle </button>
+     </div>
+    </div>
+   
+    </>
+  );
 }
 
 export default TodoSingle;
