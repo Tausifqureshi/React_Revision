8ceb962ab18c5846d72b95524db33bdc63b05942
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-26 09:42:12 +0530

    Redux-Thunk

--- src/Redux Thunk/thunkSlice.jsx
+++ src/Redux Thunk/thunkSlice.jsx
@@ -3,7 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const thunkSlice = createSlice({
   name: "todos",
   initialState: { data: [], loading: false, error: null },
-  reducers: {},
+
+  reducers: {},  //Synchronous actions ke liye.
+
+  extraReducers: (builder)=>{ // Async thunk ke liye.
+    // builder
+    console.log(builder)
+  }
+
 });
 
 export default thunkSlice;
