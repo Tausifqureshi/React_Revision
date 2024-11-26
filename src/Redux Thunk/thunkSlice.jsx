28f1eddd9f562fa3575ba54cacb3c83c30d0b7f3
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-26 09:51:33 +0530

    Thunk

--- src/Redux Thunk/thunkSlice.jsx
+++ src/Redux Thunk/thunkSlice.jsx
@@ -4,13 +4,15 @@ const thunkSlice = createSlice({
   name: "todos",
   initialState: { data: [], loading: false, error: null },
 
-  reducers: {},  //Synchronous actions ke liye.
+  reducers: {}, //Synchronous actions ke liye.
 
-  extraReducers: (builder)=>{ // Async thunk ke liye.
-    // builder
-    console.log(builder)
-  }
-
+  extraReducers: (builder) => {// Async thunk ke liye.
+    // `builder` Redux Toolkit ka helper hai jo asynchronous actions ko handle karta hai.
+    // Yeh `pending`, `fulfilled`, aur `rejected` states ko manage karta hai.
+    
+    console.log("builder ke ander builder method mile ge", builder);
+    // Output mein builder ke methods milenge, jaise addCase, addMatcher, addDefaultCase
+  },
 });
 
 export default thunkSlice;
