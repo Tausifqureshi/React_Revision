38379afdeb7ad6ff9280ef6d43dc798f53185f59
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-26 10:43:19 +0530

    Todos

--- src/Redux Thunk/FormTodos.jsx
+++ src/Redux Thunk/FormTodos.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 
 function FormTodos() {
-const [title ,setTitle] = useState('');
-  return <div>
+  const [title, setTitle] = useState("");
+  function addTodos(e){
+    e.preventDefault()
 
-  </div>;
+  }
+
+  return (
+    <form onSubmit={addTodos}>
+      <div style={{width: "20rem", margin: "1rem"}}>
+      <input 
+        type="text"
+        name="title"
+        id="title"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <button type="submit">Add Todos </button>
+      </div>
+    </form>
+  );
 }
 
 export default FormTodos;
--- src/Redux Thunk/thunkSlice.jsx
+++ src/Redux Thunk/thunkSlice.jsx
@@ -1,4 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Thunk function jo Axios se data fetch karega
+// export const fetchDatat = createAsyncThunk("todos/fetch", async () => {
+//     try {
+//       const response = await axios.get("http://localhost:8001/todo");
+//       return response.data;  // Response ke data ko return karein
+//     } catch (error) {
+//       // Agar error ho, toh error ko return karein
+//       throw Error(error.message);
+//     }
+//   });
+
+
+export const fetchDatat = createAsyncThunk("todos/fetch", async()=>{
+const response = await axios.get("http://localhost:8001/todo");
+console.log(response);
+return response.data;  // Response ke data ko return karein
+})
 
 const thunkSlice = createSlice({
   name: "todos",
