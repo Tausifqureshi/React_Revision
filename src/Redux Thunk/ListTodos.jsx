3052a2bf4634663c8ceb049f5c469559aab4fc81
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-26 14:29:26 +0530

    Eroo

--- src/Redux Thunk/ListTodos.jsx
+++ src/Redux Thunk/ListTodos.jsx
@@ -5,8 +5,9 @@ import { fetchDatat } from "./thunkSlice";
 function ListTodos() {
   const dispatch = useDispatch();
 
-  const todos = useSelector((state) => state.myExampleTodos.data); // Todos ko store se select karenge
-  console.log("initial state mil ri hai", todos);
+  const { data:todos, loading, error } = useSelector((state) => state.myExampleTodos);
+//   const todos = useSelector((state) => state.myExampleTodos.data); // Todos ko store se select karenge
+//   console.log("initial state mil ri hai", todos);
 
   useSelector((state)=>{
       console.log("state mil ri hai", state); // jo hamne todos db.json ka datat push kiya hai na slice me ohi data mile ga yaha upper todos me us me de [data, error, loading] us ko destucturing kar ke nikal sakte hai.
@@ -16,6 +17,8 @@ function ListTodos() {
     dispatch(fetchDatat()); // Thunk function ko dispatch karenge
   }, [dispatch]); // dispatch ko dependency array mein dena zaroori hai
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
   return <div>
     <h1>Todos List</h1>
       <ul>
