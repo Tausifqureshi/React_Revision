fa584dc15976d9dfc80b9692b1f53d4eacccbdd2
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-27 11:03:15 +0530

    Redux

--- src/Redux Thunk/ToolKitThorySetup.jsx
+++ src/Redux Thunk/ToolKitThorySetup.jsx
@@ -1,5 +1,8 @@
-// ========================================== ToolKit with ReduxThunk ============================================ //
-// ChatGPT Se Liya Hua Syntex ---> 
+// ======================================= Redux Toolkit Me Redux Thunk ========================================= //
+// ChatGPT Se Liya Hua Syntex --->  Redux Toolkit Me Redux Thunk
+// Redux Toolkit, jo Redux ka modern way hai, apne saath Redux Thunk middleware ko default include karta hai. Jab aap configureStore function ka use karte hain, to Redux Thunk pehle se hi setup hota hai.
+
+// Aapko Redux Thunk ko manually add karne ki zarurat nahi hoti.
 
 
 
