f7c1023ce3f901b5214ab93d44a121899205e49a
Tausifqureshi <tausifqureshi504@gmail.com>
2025-04-25 06:03:50 +0530

    Al

--- src/Z.jsx
+++ src/Z.jsx
@@ -78,6 +78,7 @@
 // validtion --> yap library 
 
 import React from 'react'
+
 const Z = () => {
   return (
     <div>
--- src/ZZZ.jsx
+++ src/ZZZ.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-
-import React from "react";
-
 function ZZZ() {
-  return <div>ZZZ
-   <h1> Lorem </h1>   
-                <p></p>
-  </div>;
-}  
+ 
+  const firstName = "Quraishi";
+  const lastName = (name) => {
+    return name;
+  };
+  // const fullName = `${firstName} ${lastName("Tausif")}`;
+  // console.log("Full Name:", fullName);
+
+  return (
+    <> 
+      <h1 onClick={()=>{ lastName("Tausif") }}>ZZZ</h1>
+      <h1>My name is  {firstName}</h1>            
+    </>
+  );
+}
 
 export default ZZZ;
-    
