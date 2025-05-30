e332fb3bb04ffda8d5a5a7fcd759ecf70153cb61
Tausifqureshi <tausifqureshi504@gmail.com>
2025-05-30 09:36:17 +0530

    UseRef

---
+++ src/useRefHook/UseRef.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from "react";
+
+
+
+function ToggleInputVisibility() {
+  const inputRef = useRef(null);
+
+  const toggleVisibility = () => {
+    const input = inputRef.current;
+    input.style.display = input.style.display === 'none' ? 'block' : 'none';
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="Toggle me" />
+      <button onClick={toggleVisibility}>Toggle Input</button>
+    </div>
+  );
+}
+
+
+
+// export default UseRef;
+
