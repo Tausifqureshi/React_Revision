01018ec961870c7292dd2e4cebd56f29dc42b0fd
Tausifqureshi <tausifqureshi504@gmail.com>
2025-06-13 09:57:32 +0530

    Use

--- src/Z.jsx
+++ src/Z.jsx
@@ -7,75 +7,12 @@
 
 
 
-import React, { useState, useEffect } from "react";
-
-function Z() {
-  const defaultValues = {
-    html: false,
-    css: false,
-    js: false,
-   
-  };
-
-  const [checkedItems, setCheckedItems] = useState(() => {
-    const saved = localStorage.getItem("checkedItems");
-    return saved ? JSON.parse(saved) : defaultValues;
-  });
-
-  const handleChange = (e) => {
-    const { name, checked } = e.target;
-    setCheckedItems((prev) => ({
-      ...prev,
-      [name]: checked,
-    }));
-  };
-
-  useEffect(() => {
-    localStorage.setItem("checkedItems", JSON.stringify(checkedItems));
-  }, [checkedItems]);
-
-  return (
-    <div>
-      <h2>🔘 Object Based</h2>
-
-      <label>
-        <input
-          type="checkbox"
-          name="html"
-          checked={checkedItems.html}
-          onChange={handleChange}
-        />
-        HTML
-      </label>
-      <br />
-
-      <label>
-        <input
-          type="checkbox"
-          name="css"
-          checked={checkedItems.css}
-          onChange={handleChange}
-        />
-        CSS
-      </label>
-      <br />
-
-      <label>
-        <input
-          type="checkbox"
-          name="js"
-          checked={checkedItems.js}
-          onChange={handleChange}
-        />
-        JavaScript
-      </label>
-
-
-      <p>Selected Items: {Object.keys(checkedItems).map((item) => item.toUpperCase()).join(", ")} </p>
-    </div>
-  );
+import React from 'react'
+function Z(){
+  return(
+    <>
+      <h1> Parctice</h1>
+    </>
+  )
 }
-
 export default Z
-
-
