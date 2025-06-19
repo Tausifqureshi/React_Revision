91dafc21b8438fa5297d7ff88fbba9de2ca5494c
Tausifqureshi <tausifqureshi504@gmail.com>
2025-06-19 12:17:05 +0530

    Users

--- src/ZZZ.jsx
+++ src/ZZZ.jsx
@@ -1,30 +1,3 @@
-// import React, { version } from "react";
-
-// function ZZZ() {
-//   const firstName = "Quraishi";
-//   const lastName = (name) => {
-//     return name;
-//   };
-//   const fullName = `${firstName} ${lastName("Tausif")}`;
-//   console.log("Full Name:", fullName);
- 
-//   return (
-//     <>
-//       <h1
-//         onClick={() => {
-//           lastName("Tausif");
-//         }}
-//       >
-//         ZZZ
-//       </h1>
-//       <h1>My name is {firstName}</h1>
-//       <h1>React {version}</h1>
-//     </>
-//   ); 
-// }
-
-// export default ZZZ;
- 
 
 
 
